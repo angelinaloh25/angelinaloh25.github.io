f36ac4adf069a46e37ed5e2ca961767c693336ab
angloh <anloh@ucsd.edu>
2026-04-11 23:41:14 -0700

    Add home, about, contact page and navbar

---
+++ transitions.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $("body").css("opacity", 0).animate({ opacity: 1 }, 200);
+
+  $(document).on("click", "a[href]", function (e) {
+    const href = $(this).attr("href");
+    if (href.startsWith("http") || href.startsWith("#")) return;
+    e.preventDefault();
+    $("body").animate({ opacity: 0 }, 200, function () {
+      window.location.href = href;
+    });
+  });
+});
